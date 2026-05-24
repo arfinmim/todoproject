6b2ea74443bf43cbdeb72a005afc09a70e713f09
arfinmim <arfinhoquemim@gmail.com>
2026-05-24 21:49:34 +0600

    Completed Django To-Do project with models, views, templates

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def home(request):
+    tasks = Task.objects.all()
+    return render(request, "home.html", {"tasks": tasks})
+
+def about(request):
+    return render(request, "about.html")
